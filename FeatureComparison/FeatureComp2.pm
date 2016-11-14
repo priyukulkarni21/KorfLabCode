7427e509fe0abcebf73d2ca25bcdd0b6170984b0
Priyanka <priyukulkarni21@gmail.com>
2016-11-13 17:48:43 -0800

    edits

--- FeatureComparison/FeatureComp2.pm
+++ FeatureComparison/FeatureComp2.pm
@@ -1,7 +1,7 @@
 package FeatureComp2;
 ## FeatureComparison has the overlap function to compare two features and check if they intersect
 
-#use strict; use warnings;
+use strict; use warnings;
 
 # A feature is simply a hash reference.
 #my $feature = {
@@ -12,7 +12,7 @@ package FeatureComp2;
 
 sub overlap {
         my ($f1, $f2) = @_;
-        if ($f1->{chr} == $f2->{chr}){
+        if ($f1->{chrom} eq $f2->{chrom}){
 
                 if (($f1->{beg} <= $f2->{beg}) and ($f1->{end} >= $f2->{beg})){return 1;}
                 elsif(($f1->{beg} >= $f2->{beg}) and ($f1->{beg} <= $f2->{end})){return 1;}
--- FeatureComparison/FileBased2.pl
+++ FeatureComparison/FileBased2.pl
@@ -18,10 +18,11 @@ while(<$in>){
 }
 close $in;
 	
+
+
 ## Read files in and compare
 
-my %feature;
-my %feature2;
+
 my $find = 0;
 my @arr;
 for (my $i = 0; $i <= $count; $i++){		# for each of the lines in first bed file
@@ -29,33 +30,35 @@ for (my $i = 0; $i <= $count; $i++){		# for each of the lines in first bed file
 	my $chr1;
 	my $start1;
 	my $stop1;
+	my %feature;
 	open (my $in, "<", $ARGV[0]);		# open the file
 	while(<$in>){				
         	chomp;
 		my $line = $_;
 		(($chr1, $start1, $stop1) = split(/\t/, $line)) if ($. == $i);	# split on tabs and save in array if line number matches the counter
-		%feature = (					# set up the hash
-			chr => $chr1, 		
+		 %feature = (					# set up the hash
+			chrom => $chr1, 		
 			beg => $start1,
 			end => $stop1,
 			);
 	}
 	close $in;
 
+	my %feature2;
 	open (my $in2, "<", $ARGV[1]) or die "error opening $ARGV[1] for reading";	# open file 2
 	while (<$in2>){
 		chomp;
 		my ($chr2, $start2, $stop2) = split(/\t/, $_);	# split each line and save in hash
 		%feature2 = (
-			chr => $chr2,
+			chrom => $chr2,
 			beg => $start2,
 			end => $stop2,
 			);
 		
-		$find = FeatureComp2::overlap(\%feature, \%feature2);	# call overlap in FeatureComp2 package. returns 0 or 1.
-
+		$find = overlap(\%feature, \%feature2);	# call overlap in FeatureComp2 package. returns 0 or 1.
+			
 		if ($find == 1){	# if find is 1, which means found an overlap, then push onto array that feature from file 1
-			my $str = "$feature{chr}\t$feature{beg}\t$feature{end}";
+			my $str = "$feature{chrom}\t$feature{beg}\t$feature{end}";
 			push (@arr, $str);
 			$find = 0;	# reset find;
 		}		
@@ -70,4 +73,12 @@ print "@arr2","\n";	# print
 
 
 
-		
+sub overlap {
+         my ($f1, $f2) = @_;
+	 if ($f1->{chrom} eq $f2->{chrom}){
+                 if (($f1->{beg} <= $f2->{beg}) and ($f1->{end} >= $f2->{beg})){return 1;}
+                 elsif(($f1->{beg} >= $f2->{beg}) and ($f1->{beg} <= $f2->{end})){return 1;}
+                 else {return 0;}
+          }
+}       
+ 
