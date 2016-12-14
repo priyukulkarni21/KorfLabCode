d8490b02408a59cb098e175c85935c71eb7741e6
Priyanka <priyukulkarni21@gmail.com>
2016-12-13 18:03:05 -0800

    changed var names mainly

--- FeatureComparison/DoubleArrayBasedLS.pl
+++ FeatureComparison/DoubleArrayBasedLS.pl
@@ -6,32 +6,35 @@ use List::MoreUtils qw/ uniq /;
 ## Double array-based linear search
 ## Read one file into an array. Open the other file and compare each feature to the array of features.
 
-die "usage: ./DoubleArrayBasedLS.pl <bed1> <bed2>" unless @ARGV ==2 ;
+die "usage: ./ArrayBasedLS.pl <bed1> <bed2>" unless @ARGV ==2 ;
 
 my ($inp, $inp2) = @ARGV;
 
-my @arr1 = @{FeatureComp2::read_bed($inp)}; 		# read bed files into arrays of hashes
-my @arr2 = @{FeatureComp2::read_bed($inp2)};
+my $features1 = FeatureComp2::read_bed($inp); 		# read bed files into arrays of hashes
+my $features2 = FeatureComp2::read_bed($inp2);
+
+
+# read bed file into array of hashes
+# return a reference to the array
+
 
 # so it's an array of hashes. I want to read that
-my $find = 0;
-my @newarr;
 
-for (my $i = 0; $i < scalar(@arr1); $i++){
-	for (my $j = 0; $j < scalar(@arr2); $j++){
-		my $find = FeatureComp2::overlap(\%{$arr1[$i]}, \%{$arr2[$j]});  #call overlap in FeatureComp2 package
-		if ($find == 1){		# if overlaps then push first file's feature into newarr
-			my $str = "$arr1[$i]->{chrom}\t$arr1[$i]->{beg}\t$arr1[$i]->{end}";
-			push @newarr, $str;
-			$find = 0;
+
+for (my $i = 0; $i < @$features1; $i++){
+	my $f1 = $features1->[$i];
+	my $find =0;
+	my $str = "";
+	for (my $j = 0; $j < @$features2; $j++){
+		my $f2 = $features2->[$j];
+		if (FeatureComp2::overlap($f1, $f2)){  #call overlap in FeatureComp2 package. # if overlaps then push first file's feature into newarr
+			$str = "$f1->{chrom}\t$f1->{beg}\t$f1->{end}";
+			$find = 1;
 		}
 	}
+	if ($find ==1){
+	print $str, "\n";
+	}
 }
-
-## print unique arr 
-$"  = "\n";
-my @unarr = uniq(@newarr);
-print "@unarr\n";
-
 
 
